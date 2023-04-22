ee03b9f7e2698a94acc0c9fdf1a9d31df7a8a1ac
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-04-22 23:31:34 +0300

    Сверстать header

--- movies-explorer-frontend/src/components/App/App.js
+++ movies-explorer-frontend/src/components/App/App.js
@@ -1,6 +1,8 @@
-//import logo from '../../images/logo.svg';
+
 //cover
 import React from 'react';
+// import { Route } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 
@@ -8,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-
     </div>
   );
 }
--- movies-explorer-frontend/src/components/Header/Header.js
+++ movies-explorer-frontend/src/components/Header/Header.js
@@ -1,17 +1,16 @@
-//import logo from '../../images/logo.svg';
 import React from 'react';
+import logo from '../../images/logo.png';
+// import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header({ handleClick }) {
   return (
-    <header className="Header">
-      <h1>HHHHHH</h1>
-      <button className='button button_up' onClick={handleClick}>
-        Регистрация
-      </button>
-      <button className='button button_login' onClick={handleClick}>
-        Войти
-      </button>
+    <header className="header">
+      <img className='header__logo' alt="Логотип сайта. Черный круг." src={logo} />
+      <ul className='header__list'>
+        <li className='header__element'><a href='https://ya.ru/' className='header__link header__link_type_up'>Регистрация</a></li>
+        <li className='header__element header__element_type_in'><a href='https://ya.ru/' className='header__link header__link_type_in'>Войти</a></li>
+      </ul>
     </header>
   );
 }
