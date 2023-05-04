ae269870924386785bbf10fcf8cabbd7e55fb562
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-05-04 13:12:07 +0300

    Верстка menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,35 +6,24 @@ import '../Animation/Animation.css';
 
 function Navigation() {
   return (
-    <nav className='menu'>
-      <ul className='menu__list'>
-        <li>
-          <Link className='menu__link menu__link-movies animation' to='/movies'>
-            Фильмы
-          </Link>
-        </li>
+    <>
+      <nav className='menu'>
+        <ul className='menu__list'></ul>
+        <ul className='menu__list'>
+          <li><Link className='menu__link menu__text-bold animation' to='/movies'>Фильмы</Link></li>
+          <li><Link className='menu__link menu__text animation' to='/saved-movies'>Сохранённые фильмы</Link></li>
+        </ul>
+        <ul className='menu__list'>
+          <li><Link className='menu__link menu__text-bold animation' to='/profile'>Аккаунт</Link></li>
+          <li className='menu__avatar-container'><Link className='menu__link animation' to='/profile'><img className='menu__avatar' alt="Иконка аватара. Силуэт портрета человечика." src={Avatar} /></Link></li>
+        </ul>
+      </nav>
 
-        <li>
-          <Link className='menu__link menu__link-save-movies animation' to='/saved-movies'>
-            Сохранённые фильмы
-          </Link>
-        </li>
-      </ul>
+      {/* <div className='menu__burger'>
+        <span className='menu__burger-line'></span>
+      </div> */}
 
-      <ul className='menu__list'>
-        <li>
-          <Link className='menu__link menu__link-account animation' to='/profile'>
-            Аккаунт
-          </Link>
-        </li>
-
-        <li className='menu__avatar-container animation'>
-          <Link className='animation' to='/profile'>
-            <img className='menu__avatar' alt="Иконка аватара. Силуэт портрета человечика." src={Avatar} />
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    </>
   );
 }
 
