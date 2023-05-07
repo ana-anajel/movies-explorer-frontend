6e694c7a05d3fe364a920a297d687f3098763ec9
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-05-07 11:22:57 +0300

    Поправить кнопку

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -15,12 +15,12 @@ function Navigation({ handleBurgerClick }) {
           <NavLink to='/' className='menu__link menu__link-span animation' activeClassName="active">Главная</NavLink>
         </span>
         <ul className='menu__list menu__list-movies'>
-          <li className='menu__link-movies'><NavLink className='menu__link menu__text-movies animation' activeClassName="active" to='/movies'>Фильмы</NavLink></li>
-          <li className='menu__link-movies'><NavLink className='menu__link menu__text-save-movies animation' activeClassName="active" to='/saved-movies'>Сохранённые фильмы</NavLink></li>
+          <li className='menu__link-movies animation'><NavLink className='menu__link menu__text-movies' activeClassName="active" to='/movies'>Фильмы</NavLink></li>
+          <li className='menu__link-movies animation'><NavLink className='menu__link menu__text-save-movies' activeClassName="active" to='/saved-movies'>Сохранённые фильмы</NavLink></li>
         </ul>
         <ul className='menu__list'>
-          <li><NavLink className='menu__link menu__text-profile animation' activeClassName="active" to='/profile'>Аккаунт</NavLink></li>
-          <li className='menu__avatar-container'><Link className='menu__link animation' to='/profile'><img className='menu__avatar' alt="Иконка аватара. Силуэт портрета человечика." src={Avatar} /></Link></li>
+          <li className='animation'><NavLink className='menu__link menu__text-profile' activeClassName="active" to='/profile'>Аккаунт</NavLink></li>
+          <li className='menu__avatar-container animation'><Link className='menu__link' to='/profile'><img className='menu__avatar' alt="Иконка аватара. Силуэт портрета человечика." src={Avatar} /></Link></li>
         </ul>
       </nav >
     </>
